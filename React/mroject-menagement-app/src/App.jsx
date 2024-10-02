efe096dc2785a1a28c4c6a08bb73d3459cc66a4b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-10-02 18:26:05 +0300

    Add projectsState and handleStartAddProject function

--- React/mroject-menagement-app/src/App.jsx
+++ React/mroject-menagement-app/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState((state) => {
+      return {
+        ...state,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  let addingProjectStatus;
+
+  if (projectState.selectedProjectId === undefined) {
+    addingProjectStatus = <NoProjectSelected onAddProject={handleStartAddProject} />;
+  } else if (projectState.selectedProjectId === null) {
+    addingProjectStatus = <NewProject onAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onAddProject={handleStartAddProject} />
+      {addingProjectStatus}
     </main>
   );
 }
