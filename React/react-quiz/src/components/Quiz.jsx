2ea988550060b81da21cf1d5ae1eea8eeb6fcc1f
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-10-09 10:41:56 +0300

    Setting Different Timers Based On The Selected Answer

--- React/react-quiz/src/components/Quiz.jsx
+++ React/react-quiz/src/components/Quiz.jsx
@@ -1,9 +1,8 @@
 import { useCallback, useState } from "react";
 
 import QUESTIONS from '../questions.js';
-
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -28,28 +27,14 @@ export default function Quiz() {
         );
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
-
     return (
         <div id="quiz">
-            <div id="question">
-                <QuestionTimer
-                    key={activeQuestionIndex}
-                    timeout={10000}
-                    onTimeout={handleSkipAnswer}
-                />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
-                                {answer}
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Question
+                key={activeQuestionIndex}
+                index={activeQuestionIndex}
+                onSkipAnswer={handleSkipAnswer}
+                onSelectAnswer={handleSelectAnswer}
+            />
         </div>
     );
 }
