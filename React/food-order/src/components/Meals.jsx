21db561cea7ee684d4f14f5dd157888fe78b68ae
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-10-11 16:36:40 +0300

    Fetch data using useHttp hook

--- React/food-order/src/components/Meals.jsx
+++ React/food-order/src/components/Meals.jsx
@@ -1,30 +1,29 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const requestConfig = {};
 
 export default function Meals() {
-    const [meals, setMeals] = useState([]);
+  const {
+    data: meals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-    async function fetchMeals() {
-        const response = await fetch('http://localhost:3000/meals');
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-        if (!response.ok) {
-            return;
-        }
-        const fetchedMeals = await response.json();
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
 
-        return fetchedMeals;
-    }
-
-    useEffect(() => {
-        (async () => {
-            const loadedMeals = await fetchMeals();
-            setMeals(loadedMeals);
-        })()
-    }, []);
-
-    return (
-        <ul id="meals">
-            {meals.map((meal) => <MealItem key={meal.id} meal={meal} />)}
-        </ul>
-    );
+  return (
+    <ul id="meals">
+      {meals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
--- React/food-order/src/contexts/CartContext.jsx
+++ React/food-order/src/contexts/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -51,6 +52,8 @@ function cartReducer(state, action) {
     }
 
     return { ...state, items: updatedItems };
+  } else if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
   }
 }
 
@@ -65,10 +68,15 @@ export function CartContextProvider({ children }) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
 
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
+
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart,
   };
 
   return (
