fb62198bfdbc32242cd2739bb95304ee309b8a10
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-10-16 15:55:35 +0300

    Add functionality to login and logout and shows different data according to isAuth state

--- React/redux-basics/src/store/index.js
+++ React/redux-basics/src/store/index.js
@@ -22,8 +22,25 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 // const counterReducer = (state = initialData, action) => {
@@ -61,5 +78,7 @@ const store = configureStore({
 // const store = createStore(counterReducer);
 
 export const counterActions = counterSlice.actions;
+
+export const authActions = authSlice.actions;
 
 export default store;
