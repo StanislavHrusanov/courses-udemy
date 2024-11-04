144dedb2a3478504ca692d991dc326039bc7da95
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-11-04 10:55:20 +0200

    Add  functions ratingAsWords and commentDateConverter to util.js

--- React/practice-routing/src/util.js
+++ React/practice-routing/src/util.js
@@ -1,14 +1,33 @@
 export const getAvgRating = (reviews) => {
-    let sumOfRatings = 0;
-    if (reviews) {
-        for (let review of reviews) {
-            sumOfRatings += review.rating;
-        }
+  let sumOfRatings = 0;
+  if (reviews) {
+    for (let review of reviews) {
+      sumOfRatings += review.rating;
     }
-    if (sumOfRatings === 0) {
-        return '';
-    }
-    const rating = Number((sumOfRatings / reviews.length));
+  }
+  if (sumOfRatings === 0) {
+    return "";
+  }
+  const rating = Number(sumOfRatings / reviews.length);
 
-    return rating.toFixed(1);
-}
+  return rating.toFixed(1);
+};
+
+export const ratingAsWords = new Map();
+ratingAsWords.set(0, "");
+ratingAsWords.set(1, "Poor!");
+ratingAsWords.set(2, "Weak!");
+ratingAsWords.set(3, "Good!");
+ratingAsWords.set(4, "Very good!");
+ratingAsWords.set(5, "Excellent!");
+
+export const commentDateConverter = (par) => {
+  const date = new Date(par);
+
+  const fullDate = date.toDateString();
+  const time = date.toTimeString();
+
+  const formatedDate = `${time.slice(0, 8)}/${fullDate}`;
+
+  return formatedDate;
+};
