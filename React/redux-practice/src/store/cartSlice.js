888274580ed7983b16134de6ecd1780f219f8986
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-11-06 14:00:29 +0200

    Add cartSlice

---
+++ React/redux-practice/src/store/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQty: 0,
+  },
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQty++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      state.totalQty--;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.price -= existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
