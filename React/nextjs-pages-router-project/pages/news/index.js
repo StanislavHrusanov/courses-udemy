f7b82ebb959bf05d7e512551288e213aaa7eda0f
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-11-19 15:06:07 +0200

    Add Linking between pages

--- React/nextjs-pages-router-project/pages/news/index.js
+++ React/nextjs-pages-router-project/pages/news/index.js
@@ -1,7 +1,20 @@
 //our-domain.com/news
+import Link from "next/link";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/one">One</Link>
+        </li>
+        <li>
+          <Link href="/news/two">Two</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
