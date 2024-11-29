ea2d8dfc35f1cc337dfb59ef5b1450c5240586a8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-11-29 12:41:34 +0200

    Add functionality to toggle AccordionItems

--- React/react-patterns-and-best-practices/src/components/Accordion/Accordion.jsx
+++ React/react-patterns-and-best-practices/src/components/Accordion/Accordion.jsx
@@ -1,3 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const AccordionCtx = createContext();
+
+export function useAccordionCtx() {
+  const ctx = useContext(AccordionCtx);
+
+  if (!ctx) {
+    throw new Error(
+      "Accordion-related components must be wrapped by <Accordion>."
+    );
+  }
+
+  return ctx;
+}
+
 export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+  const [openItemId, setOpenItemId] = useState();
+
+  function toggleItem(id) {
+    setOpenItemId((state) => (state === id ? null : id));
+  }
+
+  return (
+    <AccordionCtx.Provider value={{ openItemId, toggleItem }}>
+      <ul className={className}>{children}</ul>
+    </AccordionCtx.Provider>
+  );
 }
--- React/react-patterns-and-best-practices/src/components/Accordion/AccordionItem.jsx
+++ React/react-patterns-and-best-practices/src/components/Accordion/AccordionItem.jsx
@@ -1,8 +1,20 @@
-export default function AccordionItem({ children, className, title }) {
+import { useAccordionCtx } from "./Accordion";
+
+export default function AccordionItem({ id, children, className, title }) {
+  const { openItemId, toggleItem } = useAccordionCtx();
+
+  const isOpen = openItemId === id;
+
   return (
     <li className={className}>
-      <h3>{title}</h3>
-      <div>{children}</div>
+      <h3 onClick={() => toggleItem(id)}>{title}</h3>
+      <div
+        className={
+          isOpen ? "accordion-item-content open" : "accordion-item-content"
+        }
+      >
+        {children}
+      </div>
     </li>
   );
 }
